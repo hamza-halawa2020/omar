e1e1bd59819e8d05e5016eb8bbdb6e846b1e0b0f
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-06-19 12:44:52 +0300

    update migration

--- config/projects.php
+++ config/projects.php
@@ -3,80 +3,38 @@
 
 return [
     [
-        'name' => 'CRM',
-        'url' => env('CRM_URL', 'http://127.0.0.1:8001'),
-        'description' => 'CRM platform for customer-facing teams.',
+        'name' => 'General',
+        'url' => env('GENERAL_URL', 'http://general.upedia.test'),
+        'description' => 'Software to deliver great customer support.',
         'image' => 'assets/images/logo.png',
-    ],
-    [
-        'name' => 'HR',
-        'url' => env('HR_URL', 'http://127.0.0.1:8002'),
-        'description' => 'Secure email service for teams of all sizes.',
-        'image' => 'assets/images/logo.png',
+
     ],
     [
         'name' => 'Academy',
-        'url' => env('ACADEMY_URL', 'http://127.0.0.1:8003'),
+        'url' => env('ACADEMY_URL', 'http://academy.upedia.test'),
         'description' => 'Software to deliver great customer support.',
         'image' => 'assets/images/logo.png',
-    ],
-    [
-        'name' => 'General',
-        'url' => env('GENERAL_URL', 'http://127.0.0.1:8000'),
-        'description' => 'Software to deliver great customer support.',
-        'image' => 'assets/images/logo.png',
-     
-    ],
-    [
-        'name' => 'CRM',
-        'url' => env('CRM_URL', 'http://127.0.0.1:8001'),
-        'description' => 'CRM platform for customer-facing teams.',
-        'image' => 'assets/images/logo.png',
-    ],
-    [
-        'name' => 'HR',
-        'url' => env('HR_URL', 'http://127.0.0.1:8002'),
-        'description' => 'Secure email service for teams of all sizes.',
-        'image' => 'assets/images/logo.png',
-    ],
-    [
-        'name' => 'Academy',
-        'url' => env('ACADEMY_URL', 'http://127.0.0.1:8003'),
-        'description' => 'Software to deliver great customer support.',
-        'image' => 'assets/images/logo.png',
-    ],
-    [
-        'name' => 'General',
-        'url' => env('GENERAL_URL', 'http://127.0.0.1:8000'),
-        'description' => 'Software to deliver great customer support.',
-        'image' => 'assets/images/logo.png',
-     
-    ],
-    [
-        'name' => 'CRM',
-        'url' => env('CRM_URL', 'http://127.0.0.1:8001'),
-        'description' => 'CRM platform for customer-facing teams.',
-        'image' => 'assets/images/logo.png',
-    ],
-    [
-        'name' => 'HR',
-        'url' => env('HR_URL', 'http://127.0.0.1:8002'),
-        'description' => 'Secure email service for teams of all sizes.',
-        'image' => 'assets/images/logo.png',
-    ],
-    [
-        'name' => 'Academy',
-        'url' => env('ACADEMY_URL', 'http://127.0.0.1:8003'),
-        'description' => 'Software to deliver great customer support.',
-        'image' => 'assets/images/logo.png',
-    ],
-    [
-        'name' => 'General',
-        'url' => env('GENERAL_URL', 'http://127.0.0.1:8000'),
-        'description' => 'Software to deliver great customer support.',
-        'image' => 'assets/images/logo.png',
-     
     ],
 
-  
+    [
+        'name' => 'HR',
+        'url' => env('HR_URL', 'http://hr.upedia.test'),
+        'description' => 'Secure email service for teams of all sizes.',
+        'image' => 'assets/images/logo.png',
+    ],
+
+    [
+        'name' => 'Finance',
+        'url' => env('FINANCE_URL', 'http://finance.upedia.test'),
+        'description' => 'Software to deliver great customer support.',
+        'image' => 'assets/images/logo.png',
+    ],
+    [
+        'name' => 'CRM',
+        'url' => env('CRM_URL', 'http://crm.upedia.test'),
+        'description' => 'CRM platform for customer-facing teams.',
+        'image' => 'assets/images/logo.png',
+    ],
+
+
 ];
