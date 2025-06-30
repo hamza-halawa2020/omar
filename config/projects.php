0061c909bf1762109cf32c0c0b93ac683b6faf6b
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-06-30 15:53:23 +0000

    server

--- config/projects.php
+++ config/projects.php
@@ -4,40 +4,40 @@
 return [
     [
         'name' => 'General',
-        'url' => env('GENERAL_URL', 'http://general.upedia.test'),
+        'url' => env('GENERAL_URL', 'https://upedia-setting.thetailors.tech/'),
         'description' => 'Software to deliver great customer support.',
         'image' => 'assets/images/logo.png',
 
     ],
     [
         'name' => 'Academy',
-        'url' => env('ACADEMY_URL', 'http://academy.upedia.test'),
+        'url' => env('ACADEMY_URL', 'http://upedia-academic.thetailors.tech/'),
         'description' => 'Software to deliver great customer support.',
         'image' => 'assets/images/logo.png',
     ],
 
     [
         'name' => 'HR',
-        'url' => env('HR_URL', 'http://hr.upedia.test'),
+        'url' => env('HR_URL', 'https://upedia-hr.thetailors.tech/'),
         'description' => 'Secure email service for teams of all sizes.',
         'image' => 'assets/images/logo.png',
     ],
 
     [
         'name' => 'Finance',
-        'url' => env('FINANCE_URL', 'http://finance.upedia.test'),
+        'url' => env('FINANCE_URL', 'https://upedia-finance.thetailors.tech/'),
         'description' => 'Software to deliver great customer support.',
         'image' => 'assets/images/logo.png',
     ],
     [
         'name' => 'CRM',
-        'url' => env('CRM_URL', 'http://crm.upedia.test'),
+        'url' => env('CRM_URL', 'https://upedia-crm.thetailors.tech/'),
         'description' => 'CRM platform for customer-facing teams.',
         'image' => 'assets/images/logo.png',
     ],
     [
         'name' => 'Retention',
-        'url' => env('Retention_URL', 'http://retention.upedia.test'),
+        'url' => env('Retention_URL', 'http://upedia-retention.thetailors.tech/'),
         'description' => 'Retention platform for customer-facing teams.',
         'image' => 'assets/images/logo.png',
     ],
