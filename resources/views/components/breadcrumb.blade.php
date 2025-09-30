726fed7db6b9b2602b6cb33e5dee229962163f6e
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-10-01 00:30:56 +0300

    update

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,5 +1,5 @@
 <div class="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-24">
-    <h6 class="fw-semibold mb-0">{{ $title }}</h6>
+    <div class="fw-semibold mb-0">{{ $title }}</div>
     <ul class="d-flex align-items-center gap-2">
         <li class="fw-medium">
             <a href="" class="d-flex align-items-center gap-1 hover-text-primary">
