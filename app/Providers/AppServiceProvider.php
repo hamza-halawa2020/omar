8ca88a77bfb2b11e526537a6df67ad2f5a641a93
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-11-16 22:27:11 +0200

    update

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Providers;
+
+use Exception;
+use Google\Client;
+use Google\Service\Drive;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use Masbug\Flysystem\GoogleDriveAdapter;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+
+        $this->loadGoogleStorageDriver();
+    }
+
+    private function loadGoogleStorageDriver(string $driverName = 'google')
+    {
+        try {
+            Storage::extend($driverName, function ($app, $config) {
+                $options = [];
+
+                if (! empty($config['teamDriveId'] ?? null)) {
+                    $options['teamDriveId'] = $config['teamDriveId'];
+                }
+
+                $client = new Client;
+                $client->setClientId($config['clientId']);
+                $client->setClientSecret($config['clientSecret']);
+                $client->refreshToken($config['refreshToken']);
+
+                $service = new Drive($client);
+                $adapter = new GoogleDriveAdapter($service, $config['folder'] ?? '/', $options);
+                $driver = new Filesystem($adapter);
+
+                return new FilesystemAdapter($driver, $adapter);
+            });
+        } catch (Exception $e) {
+            info('Google Drive Storage Error: '.$e->getMessage());
+
+        }
+    }
+}
